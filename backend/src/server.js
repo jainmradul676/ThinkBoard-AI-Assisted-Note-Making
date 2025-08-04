a93db0f3b26377bdbf58d01a4eda28e8e8095e10
jainmradul676 <jainmradul38@gmail.com>
2025-08-05 00:47:00 +0530

    prepared for the depoyment on thedeployment servers

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,44 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 dotenv.config({ override: true }); // Load environment variables
 
 const PORT = process.env.PORT || 5001;
 
-// Middlewares - ordr of middlewares matters
-app.use(cors({ origin: "http://localhost:5173" })); // Enable CORS for frontend requests
-app.use(express.json()); // Middleware to parse JSON bodies - helping us fetch data from the request body otherwise it will be undefined
+const __dirname = path.resolve(); // Get the current directory name
+console.log("this is dir", __dirname);
+
+// Middlewares - order of middlewares matters
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+// Enable CORS for frontend requests
+app.use(express.json());
+
+// Middleware to parse JSON bodies - helping us fetch data from the request body otherwise it will be undefined
 // custom middleware to log request details
 // app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 // app.use((req, res, next) => {
 //   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
 //   next();
 // });
+
 app.use(rateLimiter); // Apply rate limiting middleware
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Connect to the database and start the server
 
 connectDB()
   .then(() => {
